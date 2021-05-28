1453c667ea103584d191951e8bd80817f25772d1
= <phduongit2k1@gmail.com>
2021-05-28 15:30:40 +0700

    Scroll to top button

--- asset/js/script.js
+++ asset/js/script.js
@@ -3,3 +3,16 @@ AOS.init({
     delay: 0,
     duration: 1500
 });
+
+const btnScrollUp = document.getElementById("scroll-up");
+
+window.addEventListener("scroll", () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+        btnScrollUp.style.display = "block";
+    else
+        btnScrollUp.style.display = "none";
+})
+
+btnScrollUp.addEventListener("click", () => {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+});
