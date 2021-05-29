a608a42714bbceac002eefef0fbeeefef8794a95
= <phduongit2k1@gmail.com>
2021-05-29 22:13:31 +0700

    Decorate main header, add zoom animations

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,9 +1,18 @@
 AOS.init({
-    offset: 200,
+    offset: 100,
     delay: 0,
     duration: 1500
 });
 
+// show introduction when click on swing image
+const imgSwing = document.getElementById("swing-btn");
+
+imgSwing.addEventListener("click", () => {
+    const introduction = document.getElementsByClassName("introduction")[0];
+    introduction.style.display = "block";
+});
+
+// scroll to top button
 const btnScrollUp = document.getElementById("scroll-up");
 
 window.addEventListener("scroll", () => {
@@ -15,4 +24,22 @@ window.addEventListener("scroll", () => {
 
 btnScrollUp.addEventListener("click", () => {
     window.scrollTo({top: 0, behavior: 'smooth'});
+});
+
+// click on evelope
+const imgEnvelope = document.getElementById("envelope");
+
+imgEnvelope.addEventListener("click", () => {
+    const currentSrc = imgEnvelope.getAttribute("src");
+    const isClosed = currentSrc.includes("closed");
+    const pMessage = imgEnvelope.nextElementSibling;
+
+    if (isClosed) {
+        imgEnvelope.setAttribute("src", currentSrc.replace("closed", "open"));
+        pMessage.style.display = "block";
+    }
+    else {
+        imgEnvelope.setAttribute("src", currentSrc.replace("open", "closed"));
+        pMessage.style.display = "none";
+    }
 });
